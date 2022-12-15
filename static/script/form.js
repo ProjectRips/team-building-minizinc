7c79790bb2624c1e31dab7cc99ffa0f03b3ec08b
ProjectRips <ricky.412019013@civitas.ukrida.ac.id>
2022-12-16 01:03:16 +0700

    fixing dynamic input in GUI and back-end

---
+++ static/script/form.js
@@ -0,0 +1,169 @@
+$(document).ready(function () {
+
+  disable_coach();
+  add_rmv_services();
+  add_rmv_status_max_person();
+
+
+  function disable_coach(){
+    var status = document.querySelectorAll('[name$="-status"]');
+    var coach = document.querySelectorAll('[name$="-supervise"]');
+    for (var i = 0; i < coach.length; i++) {
+      coach[i].disabled = (status[i].options.selectedIndex == 0)
+    }
+  }
+
+  function add_rmv_status_max_person(){
+    var count = $('#total_status').val();
+
+    if(count == 0 || count == ""){
+      $('#status-constraint-container"]').empty();
+    }
+    else if(count > 0){
+
+      $('#status-constraint-container').empty();
+      for(let i = 1; i <= count; i++){
+        let val_input = $("#status_" + i.toString()).val();
+        $("#status-constraint-container").append('<div id="status_max_number_'+ i.toString() +'" class="mb-3"><label for="exampleFormControlInput1" id="label_max_status_' + i.toString() + '" class="fw-bolder form-label label-white">Max Number of ' + val_input + ' in a team (Optional)</label><input value="" id="input_max_status_' + i.toString() + '" name="input_max_status[]" class="form-control" type="number" placeholder="Default input" aria-label="default input example"></div>');
+      }
+    }
+    else{
+      $('#status-constraint-container').empty();
+    }
+  }
+
+  function add_rmv_services(){
+    var count = $('#num_services').val();
+    
+    if(count == 0 || count == ""){
+      $('#service-container').empty();
+    }
+    else if(count > 0){
+      var label = '<label for="exampleFormControlInput1" class="form-label">Services</label>';
+
+      $('#service-container').empty();
+      $("#service-container").append(label);
+      for(let i = 1; i <= count; i++){
+        $("#service-container").append("<div class='form-group mb-2'><input type='text' name='services[]' value='service" + i +  "' required class='services form-control' id='mb-4' placeholder='service" + i +  "'></div>");
+      }
+    }
+    else{
+      $('#service-container').empty();
+    }
+
+  }
+
+  function add_rmv_status(){
+    var count = $('#num_status').val();
+    
+    if(count == 0 || count == ""){
+      $('#status-container').empty();
+    }
+    else if(count > 0){
+      var label = '<label for="exampleFormControlInput1" class="form-label">Statuses</label>';
+
+      $('#status-container').empty();
+      $("#status-container").append(label);
+      for(let i = 1; i <= count; i++){
+        $("#status-container").append("<div class='form-group mb-2'><input type='text' name='statuses[]' value='' required class='services form-control' id='mb-4' placeholder='Any status name" + i +  "'></div>");
+      }
+    }
+    else{
+      $('#status-container').empty();
+    }
+
+  }
+
+  var table = $('#example').DataTable({
+      columnDefs: [
+          {
+              orderable: false,
+              targets: [1, 2, 3],
+              width: '20%', targets: 0
+          },
+      ],
+      lengthMenu: [
+        [-1],
+        ['All'],
+      ],
+      responsive: true,
+      autoWidth: false,
+      scrollX:        true,
+      scrollCollapse: true,
+      fixedColumns: true
+    
+  });
+
+  $(document).on('click', '[name$="-status"]', function(){
+    disable_coach();
+  });
+
+
+  $(document).on('click', '#add-btn-pair', function(){
+    var count_no = parseInt($('#total_service_pairing').val())+1;
+    var new_input="<div class='form-group mb-2'><input type='text' name='service_pairing[]' id='service_pairing_"+count_no+"' required class='form-control' id='mb-4' placeholder='ex: A NOT B'></div>";
+    $('#service-pair-container').append(new_input);
+    $('#total_service_pairing').val(count_no)
+  });
+  
+  $(document).on('click', '#remove-btn-pair', function(){
+    var count = $('#total_service_pairing').val();
+    if(count>0){
+      $('#service_pairing_'+count).remove();
+      $('#total_service_pairing').val(count-1);
+    }
+
+  });
+
+  $(document).on('change', '#num_services', function(){
+    add_rmv_services();
+  });
+
+
+  $(document).on('click', '#add-btn-status', function(){
+    var count_no = parseInt($('#total_status').val())+1;
+    var new_input="<div id='status_"+count_no+"' class='form-group mb-2'><input value=\"\" type='text' id='status_" + count_no.toString() + "' name='statuses[]' required class='form-control' id='mb-4' placeholder='Any status name'></div>";
+
+    var new_status_input='<div id="status_max_number_'+ count_no +'" class="mb-3"><label for="exampleFormControlInput1" id="label_max_status_' + count_no + '" class="fw-bolder form-label label-white">Max Number of .. in a team (Optional)</label><input value="" id="input_max_status_' + count_no + '" name="input_max_status[]" class="form-control" type="number" placeholder="Default input" aria-label="default input example"></div>'
+    $('#status-container').append(new_input);
+    $('#status-constraint-container').append(new_status_input);
+    $('#total_status').val(count_no)
+  });
+  
+  $(document).on('click', '#remove-btn-status', function(){
+    var count = $('#total_status').val();
+    if(count>0){
+      $('#status_'+count).remove();
+      $('#status_max_number_'+count).remove();
+      $('#total_status').val(count-1);
+    }
+
+  });
+
+  $(document).on('change', 'input[id^=status_]', function(){
+    var input_num = this.id[this.id.length - 1];
+    let val_input = $(this).val();
+
+    $('#status_max_number_'+input_num).remove();
+    $("#status-constraint-container").append('<div id="status_max_number_'+ input_num.toString() +'" class="mb-3"><label for="exampleFormControlInput1" id="label_max_status_' + input_num.toString() + '" class="fw-bolder form-label label-white">Max Number of ' + val_input + ' in a team (Optional)</label><input value="" id="input_max_status_' + input_num.toString() + ' name="input_max_status[]" class="form-control" type="number" placeholder="Default input" aria-label="default input example"></div>');
+  
+  });
+
+
+
+
+  $(document).on('click', '#export-xls-btn', function() {
+      var table = $('#team-table');
+      if(table && table.length){
+        $(table).table2excel({
+          name: "Excel Document Name",
+          filename: "Team Building" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+          fileext: ".xls",
+          exclude_img: true,
+          exclude_links: true,
+          exclude_inputs: true,
+        });
+      }
+  });
+
+});
---
+++ static/script/result.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+
+  function exportReportToExcel() {
+    let tables = document.getElementById("team-table-all"); // you can use document.getElementById('tableId') as well by providing id to the table tag
+    var todaydate = new Date();  //pass val varible in Date(val)
+    var dd = todaydate .getDate();
+    var mm = todaydate .getMonth()+1; //January is 0!
+    var yyyy = todaydate .getFullYear();
+    if(dd<10){  dd='0'+dd } 
+    if(mm<10){  mm='0'+mm } 
+    var datestring = dd+'-'+mm+'-'+yyyy;
+    TableToExcel.convert(tables, { // html code may contain multiple tables so here we are refering to 1st table tag
+      name: `export_team_table_` + datestring + `.xlsx`, // fileName you could use any name
+      sheet: {
+        name: 'Sheet 1' // sheetName
+      }
+    });
+  }
+
+  function updateTeamResult() {
+    s_idx = $('#solution-select').val();
+    num_of_soln = $('#sum-soln').val();
+    for (let i = 0; i < parseInt(num_of_soln); i++) {
+      if (i == s_idx) {
+        $('#container-result-' + i.toString()).show();
+      }
+      else {
+        $('#container-result-' + i.toString()).hide();
+      }
+    }
+  }
+
+  function exportPDFFile() {
+    var todaydate = new Date();  //pass val varible in Date(val)
+    var dd = todaydate .getDate();
+    var mm = todaydate .getMonth()+1; //January is 0!
+    var yyyy = todaydate .getFullYear();
+    if(dd<10){  dd='0'+dd } 
+    if(mm<10){  mm='0'+mm } 
+    var datestring = dd+'-'+mm+'-'+yyyy;
+
+    $("#team-table-all").tableHTMLExport({
+      type: 'pdf',
+      orientation: 'p',
+      filename: `export_team_table_` + datestring + `.pdf`,
+
+    })
+  }
+
+  function exportCSVFile() {
+    var todaydate = new Date();  //pass val varible in Date(val)
+    var dd = todaydate .getDate();
+    var mm = todaydate .getMonth()+1; //January is 0!
+    var yyyy = todaydate .getFullYear();
+    if(dd<10){  dd='0'+dd } 
+    if(mm<10){  mm='0'+mm } 
+    var datestring = dd+'-'+mm+'-'+yyyy;
+
+    $("#team-table-all").tableHTMLExport({
+      // csv, txt, json, pdf
+      type: 'csv',
+
+      // default file name
+      filename: `export_team_table_` + datestring + `.csv`,
+
+      // for csv
+      separator: ',',
+      newline: '\r\n',
+      trimContent: true,
+      quoteFields: true,
+
+      // CSS selector(s)
+      ignoreColumns: '',
+      ignoreRows: '',
+
+      // your html table has html content?
+      htmlContent: false,
+
+      // debug
+      consoleLog: false,
+    })
+
+
+  }
+
+
+  $(document).on('click', '#export-xls-btn', exportReportToExcel);
+  $(document).on('click', '#export-csv-btn', exportCSVFile);
+  $(document).on('click', '#export-pdf-btn', exportPDFFile);
+  $(document).on('change', '#solution-select', updateTeamResult);
+
+
+  updateTeamResult();
+});
